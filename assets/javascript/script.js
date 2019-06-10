15224076ec9b030380336ca1cacc86554b2d4895
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-06-10 11:08:43 -0600

    backgrounds done, and carousel started

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    // Materialize carousel ===============================
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
